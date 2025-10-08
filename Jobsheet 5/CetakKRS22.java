f1c73b46f667b796982f6d4e405edddc0d85706a
Rgp-DE <radityaghathfan@gmail.com>
2025-10-08 08:49:30 +0700

    Percobaan 1 Ternary

--- Jobsheet 5/CetakKRS22.java
+++ Jobsheet 5/CetakKRS22.java
@@ -4,16 +4,13 @@ public class CetakKRS22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("-------Cetak KRS Siakad-------"); 
+        System.out.print("-------Cetak KRS Siakad-------");
         System.out.println("Apakah UKT Sudah Lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT Terverifikasi");
-            System.out.println("Silahkan Cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        String pesan = uktLunas ? "Pembayaran UKT Terverifikasi\nSilahkan Cetak KRS dan minta tanda tangan DPA"
+                : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        System.out.println(pesan);
 
         sc.close();
     }
