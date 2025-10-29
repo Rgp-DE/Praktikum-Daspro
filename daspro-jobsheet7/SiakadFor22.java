da5281955484c0f2e400c1aa12842398090ce16f
Rgp-DE <radityaghathfan@gmail.com>
2025-10-29 08:51:57 +0700

    Modifikasi

--- daspro-jobsheet7/SiakadFor22.java
+++ daspro-jobsheet7/SiakadFor22.java
@@ -7,6 +7,8 @@ public class SiakadFor22 {
         double nilai;
         double tertinggi = 0;
         double terendah = 100;
+        int lulus = 0;       // menghitung mahasiswa yang lulus (>= 60)
+        int tidakLulus = 0;  // menghitung mahasiswa yang tidak lulus (< 60)
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -17,10 +19,18 @@ public class SiakadFor22 {
             }
             if (nilai < terendah) { terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
 
         sc.close();
     }
