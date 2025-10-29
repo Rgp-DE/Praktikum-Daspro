9511a72cd9da82a81e8227e9747ba83e68b07e03
Rgp-DE <radityaghathfan@gmail.com>
2025-10-29 09:05:45 +0700

    Looping

---
+++ daspro-jobsheet7/SiakadWhile22.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SiakadWhile22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nilai, jml, i = 0;
+
+        System.out.print("Masukkan banyaknya mahasiswa: ");
+        jml = sc.nextInt();
+
+        while (i < jml) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai = sc.nextInt();
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid. Masukkan nilai yang valid!.");
+                continue;
+            }
+
+            if (nilai > 80 && nilai <=100) {
+                System.out.println ("Nilai mahassiswa ke-" + (i + 1) + " = A");
+            }else if (nilai > 73 && nilai <=80) {
+                System.out.println ("Nilai mahassiswa ke-" + (i + 1) + " = B+");
+            } else if (nilai > 65 && nilai <=73) {
+                System.out.println ("Nilai mahassiswa ke-" + (i + 1) + " = B");
+            } else if (nilai > 60 && nilai <=65) {
+                System.out.println ("Nilai mahassiswa ke-" + (i + 1) + " = C+");
+            } else if (nilai > 50 && nilai <=60) {
+                System.out.println ("Nilai mahassiswa ke-" + (i + 1) + " = C");
+            } else if (nilai > 39 && nilai <=50) {
+                System.out.println ("Nilai mahassiswa ke-" + (i + 1) + " = D");
+            } else {
+                System.out.println ("Nilai mahassiswa ke-" + (i + 1) + " = E");
+            }
+            i++;
+        }
+
+        sc.close();
+    }
+}
