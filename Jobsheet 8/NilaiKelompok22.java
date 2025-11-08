164d51cc07dba1fba5e68b3657915f2251cb194b
Rgp-DE <radityaghathfan@gmail.com>
2025-11-08 11:42:04 +0700

    Modifikasi NilaiKelompok

--- Jobsheet 8/NilaiKelompok22.java
+++ Jobsheet 8/NilaiKelompok22.java
@@ -5,23 +5,35 @@ public class NilaiKelompok22 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, totalNilai, rataNilai;
+        double rataTertinggi = 0;     // Menyimpan rata-rata tertinggi
+        int kelompokTertinggi = 0;    // Menyimpan nomor kelompok dengan rata-rata tertinggi
         int i = 1; 
 
         while (i <= 6) {
-            System.out.println("Kelompok" + i);
+            System.out.println("Kelompok " + i);
             totalNilai = 0;
 
             for (int j = 1; j <= 5; j++) {
                 System.out.print("Masukkan nilai ke-" + j + ": ");
                 nilai = sc.nextDouble();
                 totalNilai += nilai;
-                
             }
 
             rataNilai = totalNilai / 5;
-            System.out.println("Rata-rata: " + rataNilai);
+            System.out.println("Rata-rata kelompok " + i + ": " + rataNilai);
+
+            // Mengecek apakah rata-rata kelompok ini adalah yang tertinggi
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
             i++;
         }
+
+        // Menampilkan hasil akhir
+        System.out.println("\nKelompok dengan rata-rata tertinggi adalah Kelompok " 
+                            + kelompokTertinggi + " dengan nilai rata-rata " + rataTertinggi);
 
         sc.close();
     }
