b29323090e2e6d52e8993e281c5c8cd533d6264e
Rgp-DE <radityaghathfan@gmail.com>
2025-12-02 15:46:20 +0700

    Percobaan 4

---
+++ Pertemuan 14/PengunjungCafe21.java
@@ -0,0 +1,14 @@
+public class PengunjungCafe21 {
+    static void daftarPengunjung(String...namaPengunjung){
+        System.out.println("Datar Nama Pengunjung");
+        for(int i = 0; i < namaPengunjung.length; i++){
+        System.out.println("-"+namaPengunjung[i]);
+        }
+    
+    }
+    public static void main(String[] args) {
+        daftarPengunjung("Ali","Budi","Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
