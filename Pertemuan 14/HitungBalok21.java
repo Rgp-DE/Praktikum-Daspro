80f9e8ecda06496a80dde2d0915a7dbb4ea049a4
Rgp-DE <radityaghathfan@gmail.com>
2025-12-02 16:11:08 +0700

    Percobaan 5 without Function

---
+++ Pertemuan 14/HitungBalok21.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HitungBalok21 {
+    public static void main (String[] args){
+        Scanner input = new Scanner (System.in);
+        int p, l, t, L, vol;
+
+        System.out.println("Masukkan Panjang");
+        p = input.nextInt();
+        System.out.println("Masukkan Lebar");
+        l = input.nextInt();
+        System.out.println("Masukkan Tinggi");
+        t = input.nextInt();
+
+        L = p * l;
+        System.out.println("Luas Persegi panjang adalah" + L);
+        vol = p * l * t;
+        System.out.println("Volume Balok adalah" + vol);
+
+        input.close();
+       
+    }
+
+    
+}
