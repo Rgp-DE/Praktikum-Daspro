ee25481cc0b65164fdcf26cdcd174134393cd468
Rgp-DE <radityaghathfan@gmail.com>
2025-12-02 18:10:06 +0700

    Percobaan 5 with Function

--- Pertemuan 14/HitungBalok21.java
+++ Pertemuan 14/HitungBalok21.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
 
 public class HitungBalok21 {
+    static int hitungLuas(int pjg, int lb){
+        int Luas = pjg * lb;
+        return Luas;
+    }
+    static int hitungVolume(int tinggi, int a, int b){
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
     public static void main (String[] args){
         Scanner input = new Scanner (System.in);
-        int p, l, t, L, vol;
 
+        int p, l, t, L, vol;
+       
         System.out.println("Masukkan Panjang");
         p = input.nextInt();
         System.out.println("Masukkan Lebar");
@@ -12,13 +21,13 @@ public class HitungBalok21 {
         System.out.println("Masukkan Tinggi");
         t = input.nextInt();
 
-        L = p * l;
-        System.out.println("Luas Persegi panjang adalah" + L);
-        vol = p * l * t;
-        System.out.println("Volume Balok adalah" + vol);
-
+        L = hitungLuas(p, l);
+        System.out.println("Luas Persegi panjang adalah:" + L);
+        vol = hitungVolume(t, p, l);
+        System.out.println("Volume Balok adalah " + vol);
+      
         input.close();
-       
+               
     }
 
     
